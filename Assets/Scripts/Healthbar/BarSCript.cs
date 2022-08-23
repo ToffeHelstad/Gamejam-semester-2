3b4542b0ea584abd3e75b73976f2827ea2ebc542
Toffe Laptop <toffehelstad@gmail.com>
2022-08-23 22:12:01 +0200

    Timer script

--- Assets/Scripts/Healthbar/BarSCript.cs
+++ Assets/Scripts/Healthbar/BarSCript.cs
@@ -9,24 +9,47 @@ public class BarSCript : MonoBehaviour
     [SerializeField] private Image healthBar;
 
     private bool inRange = false;
-    private float currentHealth;
+    public float currentHealth;
+
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = 0f;
         UpdateHealthbar();
+        timerIsRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (inRange && Input.GetKeyDown(KeyCode.Space))
         {
-            currentHealth += 0.05f;
+            currentHealth += 0.02f;
             UpdateHealthbar();
         }
+
+        if (timerIsRunning)
+        {
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+
     }
-    
+
+
+
     private void UpdateHealthbar()
     {
         healthBar.fillAmount = currentHealth;
@@ -42,3 +65,4 @@ public class BarSCript : MonoBehaviour
         inRange = false;
     }
 }
+
