383564f0779c1086f6238b438512656262674c1e
Desert <eirik0403@gmail.com>
2022-08-24 22:18:46 +0200

    finito minigame

--- Assets/Typeracer.cs
+++ Assets/Typeracer.cs
@@ -6,46 +6,70 @@ public class Typeracer : MonoBehaviour
 {
     public GameObject textObj;
     public GameObject correctTextObj;
-    public string CorrectString = "hei";
+    public string CorrectString = "";
+    public string writtenText;
     // Start is called before the first frame update
     void Start()
     {
+        CorrectString = "hei jeg heter eplemann og jeg har en tann.";
         correctTextObj.GetComponent<TMPro.TextMeshPro>().text = "'" + CorrectString + "'";
+
+        writtenText = GetComponentInChildren<TMPro.TextMeshPro>().text;
     }
 
     // Update is called once per frame
     void Update()
     {
         ShowText();
-        CheckText();
+        CheckTextIf();
     }
 
     public void ShowText()
     {
         foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
         {
-            if (Input.GetKeyDown(vKey))
+            if (Input.GetKeyDown(vKey) && vKey != KeyCode.Return)
             {
                 print(vKey);
-                GetComponentInChildren<TMPro.TextMeshPro>().text += (char)vKey;
+                if(vKey == KeyCode.Backspace) {
+                    GetComponentInChildren<TMPro.TextMeshPro>().text = GetComponentInChildren<TMPro.TextMeshPro>().text.Substring(0, GetComponentInChildren<TMPro.TextMeshPro>().text.Length - 1);
+                } else {
+                    GetComponentInChildren<TMPro.TextMeshPro>().text += (char)vKey;
+                }
+                //if (vKey == KeyCode.I)
+                //{
+                //    StartCoroutine(TimerCheckText());
+                //}
             }
+        }
+    }
+
+    public void CheckTextIf()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            CheckText();
         }
     }
 
     public void CheckText()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        string writtenText = GetComponentInChildren<TMPro.TextMeshPro>().text;
+        Debug.Log(writtenText.GetType());
+        Debug.Log(CorrectString.GetType());
+        print(writtenText);
+        print(CorrectString);
+        //writtenText += " ";
+        if (writtenText == CorrectString)
         {
-            string writtenText = GetComponentInChildren<TMPro.TextMeshPro>().text;
-            Debug.Log(writtenText.GetType());
-            Debug.Log(CorrectString.GetType());
-            print(writtenText);
-            print(CorrectString);
-            if (GetComponentInChildren<TMPro.TextMeshPro>().text == CorrectString)
-            {
-                print("O");
-                textObj.GetComponent<TMPro.TextMeshPro>().text = "nice";
-            }
+            print("O");
+            textObj.GetComponent<TMPro.TextMeshPro>().text = "nice";
         }
     }
+
+    //IEnumerator TimerCheckText()
+    //{
+    //    yield return new WaitForSeconds(2);
+    //    CheckText();
+    //}
 }
