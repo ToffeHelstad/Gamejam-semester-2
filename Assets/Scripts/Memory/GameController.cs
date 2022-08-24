0160e0ec7ec20524d57ed6c80fa6845ef93255cc
Adam Urio <adamurio71@gmail.com>
2022-08-24 18:22:01 +0200

    Heading home
    ph-assets, reorganized folders and updated scripts

--- Assets/Scripts/Memory/GameController.cs
+++ Assets/Scripts/Memory/GameController.cs
@@ -5,11 +5,36 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite bgImage;
+
+    public Sprite[] puzzles;
+
+    public List<Sprite> gamePuzzles = new List<Sprite>();
+
     public List<Button> btns = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
+
+
+    private void Awake()
+    {
+        puzzles = Resources.LoadAll<Sprite>("Memory/Sprites/piecesYellow");
+    }
 
     private void Start()
     {
         GetButtons();
+        AddListeners();
+        AddGamePuzzles();
     }
 
     void GetButtons()
@@ -19,6 +44,40 @@ public class GameController : MonoBehaviour
         for(int i = 0; i < objects.Length; i++)
         {
             btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
         }
+    }
+
+    void AddGamePuzzles()
+    {
+        int looper = btns.Count;
+        int index = 0;
+
+        for (int i = 0; i < looper; i++)
+        {
+            if(index == looper / 2)
+            {
+                index = 0;
+            }
+
+            gamePuzzles.Add(puzzles[index]);
+
+            index++;
+        }
+
+    }
+
+    void AddListeners()
+    {
+        foreach (Button btn in btns)
+        {
+            btn.onClick.AddListener(() => PickAPuzzle());
+        }
+    }
+
+    public void PickAPuzzle()
+    {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log("You Are Clicking A Button Named" + name);
     }
 }
